04de75d3d4bad455a58c78fb69cd287f53bd3d7b
Edith M <edith.mejia@gmail.com>
2021-10-25 09:57:28 -0500

    Responsive menu tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,41 @@
 import React, { useState } from 'react';
 import styles from '../styles/Header.module.css';
 import { FaBars, FaTimes, FaSearch, FaShoppingCart, FaUserAlt } from 'react-icons/fa';
+import cx from 'classnames';
 
 const Header = () => {
 
     // Setting up menu logic
     const [isMenuActive, setMenuActive] = useState(false);
-    console.log(isMenuActive);
+    const [searchActive, setSearchActive] = useState(false);
 
     const ToggleMenu = () => {
       setMenuActive(true); 
      };
 
+    const ToggleCancel = () => {
+      setMenuActive(false); 
+    }
 
+    const ToggleSearch = () => {
+        setSearchActive(true);
+    }
 
 
     return (
         <>
             <nav>
-            <div className={styles["menu-icon"]}>
+            <div className={cx(styles["menu-icon"], 
+                            isMenuActive ? styles["hide"] : "") 
+                            }>
                 <span><FaBars onClick={ToggleMenu} /></span>
             </div>
             <div className={styles.logo}>
                 <a href="/">NAKAMA</a>
             </div>
-            <div className={styles["nav-items"]}>
+            <div className={cx(styles["nav-items"], 
+                            isMenuActive ? styles["nav-items-active"] : ""
+                            )}>
           
                     <li><a href="/">Home</a></li>
                     <li><a href="/#about">About</a></li>
@@ -32,13 +43,18 @@ const Header = () => {
                     <li><a href="/allproducts">All our products</a></li>
 
             </div>
-            <div className={styles["search-icon"]}>
-                <span><FaSearch  /></span>
+            <div className={cx(styles["search-icon"]
+                           
+                
+                            )}>
+                <span><FaSearch onClick={ToggleSearch} /></span>
             </div>
-            <div className={styles["cancel-icon"]}>
-                <span><FaTimes  /></span>
+            <div className={cx(styles["cancel-icon"], 
+                            isMenuActive ? styles["show"] : styles["hide"]
+                            )}>
+                <span><FaTimes onClick={ToggleCancel}  /></span>
             </div>
-            <form action="#">
+            <form action="#" className={cx(searchActive ? styles["form-active"] : "")}>
                 <input type="search" className={styles["search-data"]} placeholder="Search" required />
                 <button type="submit"><FaSearch/></button>
             </form>
