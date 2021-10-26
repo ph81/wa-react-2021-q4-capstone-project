8ee59ab6455c9e823f471043faa8b3949fca9aa6
Edith M <edith.mejia@gmail.com>
2021-10-26 13:55:23 -0500

    routes updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useRoutes } from 'hookrouter';
-import routes from './helpers/Navigation'
+//import { useRoutes } from 'hookrouter';
+import Routes from './helpers/Routes';
 
 const App = () => {
-  const routeResult = useRoutes(routes);
-  return routeResult;
+  return (
+    <Routes />
+  )
 }
 
 const rootElement = document.getElementById("root");
