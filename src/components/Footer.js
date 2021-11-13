45a380602714658830d1f87c27884424e8c2a1c2
Edith M <paperhearts81@gmail.com>
2021-11-12 19:44:53 -0600

    more styling tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer>
-      <h3>Ecommerce created during Wizeline’s Academy React Bootcamp.</h3>
+      <h3>Ecommerce Store Capstone project - Wizeline Academy React Bootcamp.</h3>
       <h4>Made with 🤍 by <a href="https://edithmg.netlify.app" target="_blank" rel="noopener noreferrer">Edith</a>. </h4>
       </footer>
   );
